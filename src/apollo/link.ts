61719d4d3271e86e241cca089d9b996db3866642
Laurent GAY <l.gay@sd-libre.fr>
2025-12-05 12:32:09 +0100

    remove framasoft/absinthe-socket dependancy packages - #1814

--- src/apollo/link.ts
+++ src/apollo/link.ts
@@ -1,7 +1,5 @@
-import { from, split } from "@apollo/client/core";
+import { from } from "@apollo/client/core";
 import { RetryLink } from "@apollo/client/link/retry";
-import { getMainDefinition } from "@apollo/client/utilities";
-import absintheSocketLink from "./absinthe-socket-link";
 import { authMiddleware } from "./auth";
 import errorLink from "./error-link";
 import { uploadLink } from "./absinthe-upload-socket-link";
@@ -9,23 +7,10 @@ import { removeTypenameFromVariables } from "@apollo/client/link/remove-typename
 
 const retryLink = new RetryLink();
 
-const link = split(
-  // split based on operation type
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    );
-  },
-  absintheSocketLink,
-  uploadLink
-);
-
 export const fullLink = from([
   removeTypenameFromVariables(),
   retryLink,
   errorLink,
   authMiddleware,
-  link ?? uploadLink,
+  uploadLink,
 ]);
--- src/typings/absinthe.d.ts
+++ src/typings/absinthe.d.ts
@@ -1,5 +1,1 @@
-declare module "@framasoft/socket";
-
-declare module "@framasoft/socket-apollo-link";
-
 declare module "apollo-absinthe-upload-link";
