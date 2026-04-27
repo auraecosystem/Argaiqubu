53e919a8d834257c64a20741626176d68cf60629
Sandro Santilli <strk@kbt.io>
2026-04-27 16:58:07 +0200

    Split tel: and mailto: tests

--- test/service/formatter/formatter_test.exs
+++ test/service/formatter/formatter_test.exs
@@ -126,15 +126,23 @@ defmodule Mobilizon.Service.FormatterTest do
         "<p>An article tagged with a <a class=\"hashtag\" data-tag=\"tag\" href=\"http://mobilizon.test/tag/tag\" rel=\"tag ugc\">#tag</a>.</p>"
 
       assert {^expected, [], [{"#tag", "tag"}]} = Formatter.linkify(text)
+    end
+  end
 
-      text = "tel:+1804222111"
+  describe ".add_email_links" do
+    test "turning emails into links" do
+      text = "mailto:owner@ven.ue"
 
       expected =
         "<a href=\"#{text}\" target=\"_blank\" rel=\"noopener noreferrer ugc\">#{text}</a>"
 
       assert {^expected, [], []} = Formatter.linkify(text)
+    end
+  end
 
-      text = "mailto:owner@ven.ue"
+  describe ".add_tel_links" do
+    test "turning telephone numbers into links" do
+      text = "tel:+1804222111"
 
       expected =
         "<a href=\"#{text}\" target=\"_blank\" rel=\"noopener noreferrer ugc\">#{text}</a>"
